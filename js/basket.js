9e3abd611673e5db5d9ca1a2a23b8a8e10d3a151
AlexeySvistunov <xdayx53@gmail.com>
2024-01-17 10:52:39 +0300

    added full functionality

--- js/basket.js
+++ js/basket.js
@@ -1,5 +1,10 @@
-import { getProductFromLocalStorage } from "./main.js";
+import {
+  getProductFromLocalStorage,
+  setProductToLocalStorage,
+} from "./main.js";
 import { productTemplate } from "./main.js";
+
+const productsArray = await getElementsFromServer()
 
 const cartProduct = document.querySelector(".cart");
 
@@ -20,12 +25,10 @@ async function initialServerElementsToHTML() {
   const cartProducts = products.filter((el) =>
     getProductFromLocalStorage().includes(String(el.id))
   );
-  console.log(cartProducts);
   cartProducts.forEach((product) => {
     cartProduct.insertAdjacentHTML(
-      "afterend",
+      "beforeend",
       `
-            <div class="cart">
                 <div class="cart__product">
                     <div class="cart__img">
                         <img src="${product.image}">
@@ -33,11 +36,51 @@ async function initialServerElementsToHTML() {
                     <div class="cart__title">${product.title}</div>
                     <div class="cart__del-card">X</div>
                     <div class="cart__price">${product.price}</div>
+                    <span class="cart__item-id visually-hidden">${product.id}</span>
                 </div>
-            </div>
         `
     );
   });
 }
+
+function renderCart() {
+  cartProduct.innerHTML = ''
+  const products = productsArray
+  const cartProductsId = getProductFromLocalStorage()
+  const renderArrayCart = products.filter(product => cartProductsId.includes(String(product.id)))
+  renderArrayCart.forEach(product => {
+    cartProduct.insertAdjacentHTML(
+      "beforeend",
+      `
+                <div class="cart__product">
+                    <div class="cart__img">
+                        <img src="${product.image}">
+                    </div>
+                    <div class="cart__title">${product.title}</div>
+                    <div class="cart__del-card">X</div>
+                    <div class="cart__price">${product.price}</div>
+                    <span class="cart__item-id visually-hidden">${product.id}</span>
+                </div>
+        `
+    );
+  })
+}
+
+function deleteItemFromCart(id) {
+  const basket = getProductFromLocalStorage();
+  const arrayWithoutDeleted = basket.filter((el) => el !== id);
+  const newBasket = arrayWithoutDeleted;
+  setProductToLocalStorage(newBasket);
+  renderCart()
+}
+cartProduct.addEventListener("click", (e) => {
+  const self = e.target;
+  const cartItem = self.closest(".cart__product");
+  if (self.classList.contains("cart__del-card")) {
+    const id = cartItem.querySelector(".cart__item-id").textContent;
+    deleteItemFromCart(id);
+  } else return;
+});
+
 
 initialServerElementsToHTML();
